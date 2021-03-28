3b9d6ceeae7d7ad8d2fcd87e69b3018baa6d82b7
Joshua Agbeku <mawulidickson@gmail.com>
2021-03-28 19:01:23 +0000

    adder toast for notification purposes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import '../styles/globals.css'
 export default MyApp
 import Navbar from '../components/Navbar';
 
+import { Toaster } from 'react-hot-toast';
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Toaster />
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import Loader from '../components/Loader'
+import toast from 'react-hot-toast';
+
 
 export default function Home() {
   return (
     <div>
-      <Loader show/>
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast Me
+      </button>
     </div>
   )
 }
