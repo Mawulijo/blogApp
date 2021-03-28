1db4b01796197b427dde7c2cbbd3ce0417e4d021
Joshua Agbeku <mawulidickson@gmail.com>
2021-03-28 18:48:54 +0000

    Added navbar and made it global through _app.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
 export default MyApp
+import Navbar from '../components/Navbar';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
+}
