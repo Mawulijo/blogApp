171e8a50a9090a33f4cbe1bdbf3492133e6ee6de
mawulijo <mawulidickson@gmail.com>
2021-03-29 10:55:49 +0000

    hooked the auth to firebase and built a custom react hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { Toaster } from 'react-hot-toast';
-import Navbar from '../components/Navbar';
 import { UserContext } from '../lib/context';
+import Navbar from '../components/Navbar';
 import '../styles/globals.css'
+import { useUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
+  
   return (
-    <UserContext.Provider value={{user:{}, username:"mawulijo"}}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
