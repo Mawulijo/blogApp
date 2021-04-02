0327fee194792ed31d9e27c1c7a32c4db7642889
mawulijo <mawulidickson@gmail.com>
2021-04-02 13:49:52 +0000

    added Aucheck guard as top level component

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,7 +1,11 @@
-export default function AdminPostsPage({ }) {
+import AuthCheck from '../../components/AuthCheck';
+
+export default function AdminPostsPage(props) {
   return (
     <main>
-        <h1>Posts</h1>
+        <AuthCheck>
+
+        </AuthCheck>
     </main>
-  )
+  );
 }
