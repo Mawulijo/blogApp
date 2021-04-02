3d5be3590276bcb1022a215bc8c7e8489caf8406
mawulijo <mawulidickson@gmail.com>
2021-04-02 00:04:01 +0000

    added metatag for seo

--- pages/admin/[slug].tsx
+++ pages/admin/[slug].tsx
@@ -1,6 +1,9 @@
+import Metatags from "../../components/Metatags";
+
 export default function AdminPostEditPage({ }) {
   return (
     <main>
+      <Metatags title="admin page" />
         <h1>Edit Post</h1>
     </main>
   )
