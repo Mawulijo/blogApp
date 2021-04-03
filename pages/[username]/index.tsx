3a14e8e51830b123ac51130bf93685014e869b28
mawulijo <mawulidickson@gmail.com>
2021-04-03 03:00:04 +0000

    Added meta tags

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,6 +1,7 @@
 import { getUserWithUsername, postToJSON } from '../../lib/firebase';
 import UserProfile from '../../components/UserProfile';
 import PostFeed from '../../components/PostFeed';
+import Metatags from '../../components/Metatags';
 
 export async function getServerSideProps({ query }) {
   const { username } = query;
@@ -36,6 +37,7 @@ export async function getServerSideProps({ query }) {
 export default function UserProfilePage({ user, posts }) {
   return (
     <main>
+      <Metatags title={user.username} description={`${user.username}'s public profile`} />
       <UserProfile user={user} />
       <PostFeed posts={posts} />
     </main>
